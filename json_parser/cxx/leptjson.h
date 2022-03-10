4cc3ed67c8b2db8050aa88272574e3e45d24c499
nmreadelf <frank.q.kong@hotmail.com>
2022-03-10 09:37:21 +0800

    first add lept json, and implement parse false and true

---
+++ json_parser/cxx/leptjson.h
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace leptjson {
+
+typedef enum {
+  LEPT_NULL,
+  LEPT_FALSE,
+  LEPT_TRUE,
+  LEPT_NUMBER,
+  LEPT_STRINGF,
+  LEPT_ARRAY,
+  LEPT_OBJECT
+} lept_type;
+
+enum {
+  LEPT_PARSE_OK = 0,
+  LEPT_PARSE_EXPECT_VALUE,
+  LEPT_PARSE_INVALID_VALUE,
+  LEPT_PARSE_ROOT_NOT_SINGULAR
+};
+
+typedef struct {
+  lept_type type;
+} lept_value;
+
+typedef struct {
+  const char *json;
+} lept_context;
+
+static void lept_parse_whitespace(lept_context *c);
+
+static int lept_parse_value(lept_context, lept_value *v);
+
+static int lept_parse_null(lept_context *c, lept_value *v);
+
+static int lept_parse_false(lept_context *c, lept_value *v);
+
+static int lept_parse_true(lept_context *c, lept_value *v);
+
+int lept_parse(lept_value *v, const char *json);
+
+lept_type lept_get_type(const lept_value *v);
+
+} // namespace leptjson
