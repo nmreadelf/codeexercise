60f8bc790ed4f7270b8c112327bb792b80da6703
nmreadelf <frank.q.kong@hotmail.com>
2022-05-17 23:24:12 +0800

    add double pointer listnode solution

--- datastructures/merge-two-sorted-lists/solution.cc
+++ datastructures/merge-two-sorted-lists/solution.cc
@@ -27,3 +27,33 @@ public:
         return head.next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head;
+        ListNode ** cur = &head;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                *cur = list1;
+                list1 = list1->next;
+            } else {
+                *cur = list2;
+                list2 = list2->next;
+            }
+            cur = &((*cur)->next);
+        }
+        *cur = list1 == nullptr ? list2 : list1;
+        return head;
+    }
+};
