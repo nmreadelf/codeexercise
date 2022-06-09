8a3faee4e66c8757d01d06056e9a2fadde5333f3
nmreadelf <frank.q.kong@hotmail.com>
2022-06-10 05:11:07 +0800

    add high order function full_adderi

--- adder/adder.ml
+++ adder/adder.ml
@@ -21,3 +21,16 @@ let full_adderi (input_a, input_b, carry_in) =
 
 let (sum, carry_in) = full_adderi (1, 1, 1) in
 printf "%d %d\n" sum carry_in
+
+let pack_int f =
+        let i2b i = if i=0 then false else true in
+        let b2i b = if b then 1 else 0 in
+        function (a, b, c) ->
+                let x, y = f ((i2b a), (i2b b), (i2b c)) in
+                (b2i x), (b2i y);;
+
+let full_adderi = pack_int full_adder;;
+
+
+let (sum, carry_in) = full_adderi (1, 1, 1) in
+printf "%d %d\n" sum carry_in
