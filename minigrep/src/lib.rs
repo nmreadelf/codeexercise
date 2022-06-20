e1fdece722929a5a7a6f4c87b05410ff95f85240
nmreadelf <frank.q.kong@hotmail.com>
2022-06-21 01:15:12 +0800

    feat minigrep unit test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -26,3 +26,30 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
+}
