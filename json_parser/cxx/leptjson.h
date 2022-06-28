d757943ff8010e13eb51ca7b60676a4d5c5ee46a
nmreadelf <frank.q.kong@hotmail.com>
2022-06-28 23:17:26 +0800

    add abourt parse string interface

--- json_parser/cxx/leptjson.h
+++ json_parser/cxx/leptjson.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifndef LEPT_PARSE_STACK_INIT_SIZE
+#define LEPT_PARSE_STACK_INIT_SIZE 256
+#endif
+
 namespace leptjson {
 
 typedef enum {
@@ -21,13 +25,18 @@ enum {
 };
 
 typedef struct {
-  double n;
+  union {
+    struct { char* s; size_t len; } s; // string
+    double n; // number
+  } u;
   lept_type type;
 } lept_value;
 
 typedef struct {
-  const char *json;
-} lept_context;
+    const char* json;
+    char* stack;
+    size_t size, top;
+}lept_context;
 
 static void lept_parse_whitespace(lept_context *c);
 
@@ -38,10 +47,38 @@ static int lept_parse_value(lept_context, lept_value *v);
 
 static int lept_parse_number(lept_context *c, lept_value *v);
 
-double lept_get_number(const lept_value *v);
+static double lept_get_number(const lept_value *v);
+
+static void lept_set_string(lept_value* v, const char*s, size_t len);
 
 int lept_parse(lept_value *v, const char *json);
 
-lept_type lept_get_type(const lept_value *v);
+static lept_type lept_get_type(const lept_value *v);
+
+static void lept_free(lept_value* v);
+
+static void lept_init(lept_value *v) {
+	v->type = LEPT_NULL;
+}
+
+int lept_get_boolean(const lept_value* v);
+
+void lept_set_boolean(lept_value* v, int b);
+
+double lept_get_number(const lept_value* v);
+
+void lept_set_number(lept_value* v, double n);
+
+const char* lept_get_string(const lept_value* v);
+
+size_t lept_get_string_length(const lept_value* v);
+
+void lept_set_string(lept_value* v, const char* s, size_t len);
+
+static void* lept_context_push(lept_context* c, size_t size);
+
+static void* lept_context_pop(lept_context* c, size_t size);
+
+static int lept_parse_string(lept_context* c, lept_value* v);
 
 } // namespace leptjson
